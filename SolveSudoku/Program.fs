4b858fa00d592622ff89f76931043a62faab02c4
Mario Prince <mario.prince@outlook.com>
2017-04-19 19:36:00 -0400

    correction de perspective

--- SolveSudoku/Program.fs
+++ SolveSudoku/Program.fs
@@ -4,7 +4,7 @@
 let main argv = 
     printfn "%A" argv
 
-    let grid  = sudokuFromImage "C:\Users\Spock\Downloads\sudoku1.jpg"
+    let grid  = sudokuFromImage "C:\Users\Spock\Downloads\sudoku3.jpg"
     match grid with
         | Grid d -> printfn "%s" d
         | Error e -> printfn "%s" e
--- SudokuFromImage/SudokuFromImage.fs
+++ SudokuFromImage/SudokuFromImage.fs
@@ -27,53 +27,88 @@ module SudokuFromImage =
                             |> Array.filter ((<>) (-1, -1))
         HashMap [for parent in parents -> parent, pXc |> Array.filter (fun pc -> match pc with | p, _ -> p = parent)
                                                       |> Array.map (fun pc -> match pc with | _, c -> c)]
+
+    let private max (a: float32 []) : int = a |> Array.findIndex ((=) (a |> Array.max))
+    
+    let private min (a: float32 []) : int = a |> Array.findIndex ((=) (a |> Array.min)) 
+
+    let private clockWisePoints (pts : PointF []) : VectorOfPointF =
+        // top left, top right, bottom right, bottom left
+        let sum = pts |> Array.map (fun p -> p.X + p.Y)
+        let diff = pts |> Array.map (fun p -> p.X - p.Y)
+        new VectorOfPointF([| pts.[min sum]; pts.[max diff]; pts.[max sum]; pts.[min diff] |])
+                           
           
     let sudokuFromImage (path : string)  : SudokuResult =
         try
             let image = CvInvoke.Imread(path, ImreadModes.ReducedGrayscale4)
-        
-            let imCanny = new Mat()
-            CvInvoke.Canny(image, imCanny, 50.0, 150.0)
-            
-            let imDilate = new Mat()
-            CvInvoke.Dilate(imCanny, imDilate, null, Point(-1, -1), 1, BorderType.Default, MCvScalar(0.0, 0.0, 0.0))
-            
-            let contours = new VectorOfVectorOfPoint()
 
-            //let lines = CvInvoke.HoughLinesP(imDilate, 1.0, Math.PI/180.0, 50, 50.0, 10.0 );
-    
-            //let imLines = image.Clone()
-            //for line : LineSegment2D in lines do
-            //    CvInvoke.Line(imLines, line.P1, line.P2, MCvScalar(0.0, 0.0, 255.0), 3, LineType.AntiAlias);
-            
-            let hierarchie = new Mat()
-            CvInvoke.FindContours(imDilate.Clone(), contours, hierarchie, RetrType.Ccomp, ChainApproxMethod.ChainApproxSimple)
-            
-            // CvInvoke.DrawContours(image, contours, -1, MCvScalar(0.0, 0.0, 0.0), 2)
-
-            // Find the biggest contour with 81 holes 
-            let hData = MatToArrayOfArrayOfInt hierarchie
-            let parents = getParents hData
-            let parentsChilds = getParentsWithChilds hData parents
-           
-            let parents81Childs = [for parent in parents do if parentsChilds.[parent].Length >= 81 then yield parent]
-  
-            let ca = contours.ToArrayOfArray()
-            let sud = [ca.[parents81Childs.Head]] |> List.toArray
-            let carres = [for child in parentsChilds.[parents81Childs.Head] -> ca.[child]] |> List.toArray
-
-            CvInvoke.DrawContours(image, new VectorOfVectorOfPoint(carres), -1, MCvScalar(0.0, 0.0, 0.0), 2)
-            
             #if  DEBUG
             let temp = IO.Path.GetTempPath()
             CvInvoke.Imwrite(temp + "image.jpg", image) |> ignore
-            CvInvoke.Imwrite(temp + "canny.jpg", imCanny) |> ignore
-            CvInvoke.Imwrite(temp + "dilate.jpg", imDilate) |> ignore
+            #endif
+
+            let imCanny = new Mat()
+            CvInvoke.Canny(image, imCanny, 50.0, 150.0)
+
+            #if  DEBUG
+            CvInvoke.Imwrite(temp + "imCanny.jpg", imCanny) |> ignore
             #endif
             
-            Grid ""
+            let imDilate = new Mat()
+            CvInvoke.Dilate(imCanny, imDilate, null, Point(-1, -1), 1, BorderType.Default, MCvScalar(0.0, 0.0, 0.0))
 
+            #if  DEBUG
+            CvInvoke.Imwrite(temp + "imDilate.jpg", imDilate) |> ignore
+            #endif
+            
+            let contours = new VectorOfVectorOfPoint()
+
+            let hierarchy = new Mat()
+            CvInvoke.FindContours(imDilate.Clone(), contours, hierarchy, RetrType.Ccomp, ChainApproxMethod.ChainApproxSimple)
+            
+            // Find the biggest contour with 81 holes 
+            let hData = MatToArrayOfArrayOfInt hierarchy
+            let parents = getParents hData
+            let parentsChilds = getParentsWithChilds hData parents
+            let parents81 = [for parent in parents do if parentsChilds.[parent].Length >= 81 then yield parent]
+  
+            if parents81.Length = 0 then
+                Error "Pas de grille dans l'image"
+            else
+                let parents81Area = Collections.Map [for p in parents81 -> CvInvoke.ContourArea(contours.[p], false), p]
+                let parent81 = parents81Area.[parents81Area |> Map.keys |> Set.maxElement]      // biggest contour with 81 holes (or more)
+           
+                // Get the 4 corners of the grid
+                let ca = contours.ToArrayOfArray()
+                let corners = new VectorOfPoint()
+                CvInvoke.ApproxPolyDP(new  VectorOfPoint(ca.[parent81]), corners, 5.0, true)
+   //             CvInvoke.DrawContours(image, new VectorOfVectorOfPoint(corners), -1, MCvScalar(0.0, 0.0, 0.0), 2)
+                if corners.Size <> 4 then
+                    Error (sprintf "La grille a %i coins..." corners.Size)
+                else
+                    // Correct the perspective
+                    let rect = CvInvoke.BoundingRectangle(corners)
+                    let len = Math.Max(rect.Height, rect.Width)
+                    // The order of points is clockwise : top left, top right, bottom right, bottom left
+                    let dstPoints = new VectorOfPointF([| new PointF(0.0f, 0.0f);
+                                                          new PointF(float32 len, 0.0f); 
+                                                          new PointF(float32 len, float32 len); 
+                                                          new PointF(0.0f, float32 len) |])
+                    let srcPoints = [| new PointF(float32 corners.[0].X, float32 corners.[0].Y); 
+                                       new PointF(float32 corners.[1].X, float32 corners.[1].Y); 
+                                       new PointF(float32 corners.[2].X, float32 corners.[2].Y); 
+                                       new PointF(float32 corners.[3].X, float32 corners.[3].Y) |] |> clockWisePoints
+                    let m = CvInvoke.GetPerspectiveTransform(srcPoints, dstPoints)
+                    let imTrans = new Mat(len, len, DepthType.Cv8U, 1)
+                    CvInvoke.WarpPerspective(image, imTrans, m, new Size(len, len))
+                    
+                    #if  DEBUG
+                    CvInvoke.Imwrite(temp + "imTrans.jpg", imTrans) |> ignore
+                    #endif
+                   
+                    Grid ""
+        
         with
             | :?ArgumentException as ex -> Error ex.Message
-            |_ -> reraise()
-            
+            |_ -> reraise ()
