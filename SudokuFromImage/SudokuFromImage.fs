61d33ba062688db2ea85740c0c91ae0fb83ca881
Mario Prince <mario.prince@outlook.com>
2017-04-28 22:53:50 -0400

    raffinement dans la recherche de la grille

--- SudokuFromImage/SudokuFromImage.fs
+++ SudokuFromImage/SudokuFromImage.fs
@@ -38,8 +38,7 @@ module SudokuFromImage =
         let sum = pts |> Array.map (fun p -> p.X + p.Y)
         let diff = pts |> Array.map (fun p -> p.X - p.Y)
         new VectorOfPointF([| pts.[min sum]; pts.[max diff]; pts.[max sum]; pts.[min diff] |])
-                           
-          
+    
     let sudokuFromImage (path : string)  : SudokuResult =
         try
             let image = CvInvoke.Imread(path, ImreadModes.ReducedGrayscale4)
@@ -67,40 +66,44 @@ module SudokuFromImage =
             use hierarchy = new Mat()
             CvInvoke.FindContours(imDilate.Clone(), contours, hierarchy, RetrType.Ccomp, ChainApproxMethod.ChainApproxSimple)
              
-            // Find the biggest contour with 81 holes 
+            // Find the biggest square with 81 holes 
             let hData = MatToArrayOfArrayOfInt hierarchy
             let parents = getParents hData
             let parentsChilds = getParentsWithChilds hData parents
-            let parents81 = [for parent in parents do if parentsChilds.[parent].Length >= 81 then yield parent]
+            let parents81 = [for parent in parents do if parentsChilds.[parent].Length >= 81 then yield parent]    // select contours with 81 holes or more
   
             if parents81.Length = 0 then
                 Error "Pas de grille dans l'image"
             else
-                let parents81Area = Collections.Map [for p in parents81 -> CvInvoke.ContourArea(contours.[p], false), p]
-                let parent81 = parents81Area.[parents81Area |> Map.keys |> Set.maxElement]      // biggest contour with 81 holes (or more)
-           
-                // Get the 4 corners of the grid
                 let contoursA = contours.ToArrayOfArray()
-                use corners = new VectorOfPoint()
-                CvInvoke.ApproxPolyDP(new VectorOfPoint(contoursA.[parent81]), corners, 5.0, true)
+                let parents81Corners = hashMap [for p in parents81 -> 
+                                                    let corners = new VectorOfPoint()
+                                                    CvInvoke.ApproxPolyDP(new VectorOfPoint(contoursA.[p]), corners, 5.0, true)
+                                                    p, corners]  
 
-                #if  DEBUG
-                use imCorners = image.Clone()
-                CvInvoke.DrawContours(imCorners, new VectorOfVectorOfPoint(corners), -1, MCvScalar(0.0, 0.0, 0.0), 2)
-                CvInvoke.Imwrite(temp + "imCorners.jpg", imCorners) |> ignore
-                #endif
-
-                if corners.Size <> 4 then
-                    Error (sprintf "La grille a %i coins..." corners.Size)
+                let parents81Square = parents81 |> List.filter (fun p -> parents81Corners.[p].Size = 4)   // select the contours that are rectangles
+                
+                if parents81Square.IsEmpty then
+                    Error "Pas de grille dans l'image"
                 else
+                    let parents81Area = hashMap [for p in parents81 -> p, CvInvoke.ContourArea(contours.[p], false)]
+                    let parent81 = parents81Square |> List.maxBy (fun p -> parents81Area.[p])       // select the rectangle with the greatest area
+                    let corners = parents81Corners.[parent81]
+                   
+                    #if  DEBUG
+                    use imCorners = image.Clone()
+                    CvInvoke.DrawContours(imCorners, new VectorOfVectorOfPoint(corners), -1, MCvScalar(0.0, 0.0, 0.0), 2)
+                    CvInvoke.Imwrite(temp + "imCorners.jpg", imCorners) |> ignore
+                    #endif
+
                     // Correct the perspective
-                    let rect = CvInvoke.BoundingRectangle(corners)
+                    let rect = CvInvoke.BoundingRectangle(parents81Corners.[parent81])
                     let gridLen = Math.Max(rect.Height, rect.Width)     // grid side length
                     // The order of points is clockwise : top left, top right, bottom right, bottom left
-                    use dstPoints = new VectorOfPointF([| new PointF(0.0f, 0.0f);
-                                                          new PointF(float32 gridLen - 1.0f, 0.0f); 
-                                                          new PointF(float32 gridLen - 1.0f, float32 gridLen - 1.0f); 
-                                                          new PointF(0.0f, float32 gridLen - 1.0f) |])
+                    use dstPoints = new VectorOfPointF([|   new PointF(0.0f, 0.0f);
+                                                            new PointF(float32 gridLen - 1.0f, 0.0f); 
+                                                            new PointF(float32 gridLen - 1.0f, float32 gridLen - 1.0f); 
+                                                            new PointF(0.0f, float32 gridLen - 1.0f) |])
                     use srcPoints = [| for corner in corners.ToArray() -> new PointF(float32 corner.X, float32 corner.Y) |] |> clockWisePoints
                     use m = CvInvoke.GetPerspectiveTransform(srcPoints, dstPoints)
                     use imTrans = new UMat(gridLen, gridLen, DepthType.Cv8U, 1)
@@ -110,74 +113,52 @@ module SudokuFromImage =
                     CvInvoke.Imwrite(temp + "imTrans.jpg", imTrans) |> ignore
                     #endif
 
-                    //// convert grayscale image to binary image
-                    let imThresh = new UMat()
-                    CvInvoke.AdaptiveThreshold(imTrans, imThresh, 255.0, AdaptiveThresholdType.MeanC, ThresholdType.Binary, 9, 9.0)
-                                                  
+                    use imCanny2 = new UMat()
+                    CvInvoke.Canny(imTrans, imCanny2, 50.0, 150.0)
+
                     #if  DEBUG
-                    CvInvoke.Imwrite(temp + "imThresh.jpg", imThresh) |> ignore
+                    CvInvoke.Imwrite(temp + "imCanny2.jpg", imCanny2) |> ignore
                     #endif
-
-                    //let imBlur = new Mat()
-                    //CvInvoke.Blur(imThresh, imBlur, new Size(3, 3), new Point (-1, -1))
-                                                  
-                    //#if  DEBUG
-                    //CvInvoke.Imwrite(temp + "imBlur.jpg", imBlur) |> ignore
-                    //#endif 
-                    //let sLen = float imThresh.Size.Height / 9.0       // length of square side 
-                    //let sMid  = sLen / 2.0                           // middle of the square side
-                    //let sMidSmall  = sMid * 0.75     // to extract a smaller square centered in the middle of the bigger square. The goal is to extract the digit without the grid lines 
-                    //let squares = seq {for y in sMid .. sLen .. sLen * 9.0 do 
-                    //                        for x in sMid .. sLen .. sLen * 9.0 -> 
-                    //                             new UMat(imThresh, new Rectangle(new Point(int (x - sMidSmall), int (y - sMidSmall)), 
-                    //                                                             new Size(int (sMidSmall * 2.0), int (sMidSmall * 2.0)))) 
-
-                    let sideLen = float imThresh.Size.Height / 9.0       // approximative length of square side 
+            
+                    let sideLen = float imCanny2.Size.Height / 9.0   // approximative length of square side 
                     let sideMid  = sideLen / 2.0    
-                    let sideMidSmall  = sideMid * 0.9     // to extract the digit without most of the grid lines 
+                    //let sideMidSmall  = sideMid * 0.9     // to extract the digit without most of the grid lines 
                     
-                    let squares = seq {for y in sideMid .. sideLen .. sideLen * 9.0 do 
-                                            for x in sideMid .. sideLen .. sideLen * 9.0 -> 
-                                                use square = new UMat(imThresh, new Rectangle(new Point(int (x - sideMidSmall), int (y - sideMidSmall)), 
-                                                                                              new Size(int (sideMidSmall * 2.0), int (sideMidSmall * 2.0)))) 
-                                                use contours = new VectorOfVectorOfPoint()
-                                                CvInvoke.FindContours(square.Clone(), contours, null, RetrType.List, ChainApproxMethod.ChainApproxSimple)
+                    let squares = [for y in sideMid .. sideLen .. sideLen * 9.0 do 
+                                        for x in sideMid .. sideLen .. sideLen * 9.0 -> 
+                                            let square = new UMat(imCanny2, new Rectangle(new Point(int (x - sideMid), int (y - sideMid)), 
+                                                                                           new Size(int sideLen, int sideLen))) 
+                                            use contours = new VectorOfVectorOfPoint()
+                                            CvInvoke.FindContours(square.Clone(), contours, null, RetrType.List, ChainApproxMethod.ChainApproxSimple)
+                                            if contours.Size = 0 then
+                                                None
+                                            else
                                                 let contoursA = contours.ToArrayOfArray()
-                                                let contoursArea = Collections.Map [for c in contoursA -> c.Length, c]
-                                                let squareContour = contoursArea.[contoursArea |> Map.keys |> Set.maxElement]   // extract the most «convoluted» contour
-                                                let squareRect = CvInvoke.BoundingRectangle(new VectorOfPoint(squareContour))
-                                                new UMat(square, squareRect)
-
-                                       }
+                                                let digitContour = contoursA |> Array.maxBy (fun c -> c.Length)
+                                                let digitRect = CvInvoke.BoundingRectangle(new VectorOfPoint(digitContour))
+                                                Some (new UMat(square, digitRect))]
 
                     #if  DEBUG
-                    squares |> Seq.iteri (fun i s -> let fn = sprintf "ImDigits%02i.jpg" i
-                                                     CvInvoke.Imwrite(temp + fn, s) |> ignore
-                                                     s.Dispose())
+                    squares |> Seq.iteri ( fun i s -> 
+                                                match s with
+                                                | None -> ()
+                                                | Some m -> let fn = sprintf "ImDigits%02i.jpg" i
+                                                            CvInvoke.Imwrite(temp + fn, m) |> ignore)
                     #endif
-                     
-                    //use ocr = new Tesseract("", "eng", OcrEngineMode.TesseractOnly)
-                    //let grid = [for square in squares ->
-                    //               ocr.Recognize(square)
-                    //               let digits = ocr.GetCharacters() |> Array.filter (fun (ch : Tesseract.Character) -> "123456789".Contains(ch.Text))
-                    //               square.Dispose()
-                    //               if digits.Length = 0 then 
-                    //                  "."
-                    //               else 
-                    //                  digits.[0].Text
-                    //            ]
-                                
+                   
                     use ocr = new Tesseract("", "eng", OcrEngineMode.TesseractOnly, "123456789")
                     let grid = [for square in squares ->
-                                   ocr.Recognize(square)
-                                   let digits = ocr.GetCharacters()
-                                   square.Dispose()
-                                   if digits.Length = 0 then 
-                                      "."
-                                   else 
-                                      digits.[0].Text
-                                ]
+                                    match square with
+                                    | None -> "."
+                                    | Some m -> 
+                                        ocr.Recognize(m)
+                                        m.Dispose()
+                                        match ocr.GetCharacters() with
+                                        | ch when ch.Length = 0 -> "."
+                                        | ch when ch.[0].Text = " " -> "."
+                                        | ch -> ch.[0].Text]
+
                     Grid (String.concat "" grid)
         with
-            | :?ArgumentException as ex -> Error ex.Message
-            |_ -> reraise ()
+        | :?ArgumentException as ex -> Error ex.Message
+        | _ -> reraise ()
